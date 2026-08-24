ca46adcc694174397ac341108c31379e596c4a2b
migueldiazalcaraz0 <miguel.diaz.alcaraz@gmail.com>
2026-08-23 21:06:33 -0700

    finished project

--- stock-news-hard-start/main.py
+++ stock-news-hard-start/main.py
@@ -86,20 +86,7 @@ def getnews():
 getnews()
 
 
-## STEP 3: Use twilio.com/docs/sms/quickstart/python
-# Send a separate message with each article's title and description to your phone number. 
-#HINT 1: Consider using a List Comprehension.
 
+##Skipping twillio as only paid services allowed now not intrested in paying for api i wont use 
 
-
-#Optional: Format the SMS message like this: 
-"""
-# TSLA: 🔺2%
-# Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-# Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-# or
-# "TSLA: 🔻5%
-# Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-# Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-# """
-
+print("completed")
